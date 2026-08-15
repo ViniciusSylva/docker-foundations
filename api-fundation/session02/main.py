80f4c13e78b8a8572687e46089ed863e3434f8c5
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-08-15 11:35:23 -0300

    feat: implementation of if __name__ == "__main__"

--- api-fundation/session02/main.py
+++ api-fundation/session02/main.py
@@ -3,5 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def raiz():
+async def mensagem():
     return {"msg": "FastAPI boladona"}
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True) 
