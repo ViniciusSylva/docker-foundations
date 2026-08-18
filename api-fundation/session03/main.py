6c8d659d90be0b980e7c5de83840b79527d3edd6
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-08-18 06:57:23 -0300

    feat(api): implement POST requests

--- api-fundation/session03/main.py
+++ api-fundation/session03/main.py
@@ -1,6 +1,10 @@
+from typing import List, Optional
+
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
+
+from models import Curso
 
 app = FastAPI()
 
@@ -28,10 +32,18 @@ async def get_cursos():
 async def get_curso(curso_id: int):
     try: 
         curso = cursos[curso_id]
+        return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
 
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
     return curso
+
 
 if __name__ == "__main__":
     import uvicorn
