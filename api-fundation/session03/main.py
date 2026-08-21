b9fcf801ad6eceb808e3d4f86cd6f17fefb63581
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-08-21 13:53:18 -0300

    feat(backend): implement dependency injection

--- api-fundation/session03/main.py
+++ api-fundation/session03/main.py
@@ -1,8 +1,19 @@
-from typing import List, Optional
+from typing import List, Optional, Any
 
-from fastapi import FastAPI, status, HTTPException, Response, Path, Query, Header
+from fastapi import FastAPI, status, HTTPException, Response, Path, Query, Header, Depends
+
+from time import sleep
 
 from models import Curso
+
+def fake_db():
+    try:
+        print('Abrindo conexão com banco de dados...')
+        sleep(1)
+    finally:
+        print('Fechando conexão com banco de dados...')
+        sleep(1)
+
 
 app = FastAPI()
 
@@ -22,7 +33,7 @@ cursos = {
 
 
 @app.get('/cursos')
-async def get_cursos():
+async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
 
@@ -33,7 +44,7 @@ async def get_curso(
         description='Deve ser entre 1 e 2', 
         gt=0, 
         lt=3,
-    )
+    ) , db: Any = Depends(fake_db)
 ):
     try: 
         curso = cursos[curso_id]
@@ -43,14 +54,14 @@ async def get_curso(
 
 
 @app.post('/cursos', status_code=status.HTTP_201_CREATED)
-async def post_curso(curso: Curso):
+async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
     cursos[next_id] = curso
     del curso.id
     return curso
 
 @app.put('/cursos/{curso_id}')
-async def put_curso(curso_id: int, curso: Curso):
+async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
     if curso_id in cursos: 
         cursos[curso_id] = curso
         del curso.id
@@ -60,7 +71,7 @@ async def put_curso(curso_id: int, curso: Curso):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com id {curso_id}")
 
 @app.delete('/cursos/{curso_id}')
-async def delete_curso(curso_id: int):
+async def delete_curso(curso_id: int, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         del cursos[curso_id]
         
@@ -69,7 +80,7 @@ async def delete_curso(curso_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com id {curso_id}")
 
 @app.get('/calculadora')
-async def calcular(a: int = Query(gt=5), b: int = Query(default = None, gt=10), x_geek: str = Header(default = None), c: Optional[int] = None):
+async def calcular(a: int = Query(gt=5), b: int = Query(default = None, gt=10), x_geek: str = Header(default = None), c: Optional[int] = None, db: Any = Depends(fake_db)):
     soma: int = a + b
     if c:
         soma += c
