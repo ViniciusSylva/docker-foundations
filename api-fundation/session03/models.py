0cbe624754f6c784d43eec55fe468c921900b702
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-08-22 10:37:04 -0300

    feat(courses): add course imports to a list

--- api-fundation/session03/models.py
+++ api-fundation/session03/models.py
@@ -8,3 +8,9 @@ class Curso(BaseModel):
     titulo: str
     aulas: int
     horas: int
+
+
+cursos = [
+    Curso(id=1, titulo='Programação para Leigos', aulas=42, horas=56),
+    Curso(id=2, titulo='Algoritimos e lógica de programação', aulas=65, horas=97)
+]
